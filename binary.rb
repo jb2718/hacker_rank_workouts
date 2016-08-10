e51ee01fd1398d8c2fbff0442154bb10ea8f1021
jb2718-SE <jbattle2718@gmail.com>
2016-08-10 12:44:34 -0400

    added method to manually calculate binary string rep of base 10 digit

--- binary.rb
+++ binary.rb
@@ -5,10 +5,10 @@ require 'pry'
 # Then find and print the base-10 integer denoting the 
 # maximum number of consecutive 1's in n's binary representation.
 
-def consecutive_ones(num)
+def consecutive_ones(digits)
   tally = []
   count = 0
-  digits = num.to_s.chars.map(&:to_i)
+  # digits = num.to_s.chars.map(&:to_i)
   digits.each do |digit|
     if digit == 0
       tally << count
@@ -21,7 +21,40 @@ def consecutive_ones(num)
   tally
 end
 
-def base10_to_base2(base_10_num)
+def highest_power_of_2(num)
+  power = Math.sqrt(num).floor
+  half = num
+  loop do
+    return power if num == 2**power
+    if 2**power > num
+      half /= 2
+      power = Math.sqrt(half).floor
+    else
+      if 2**(power+1) > num
+        return power
+      else
+        power += 1
+      end
+    end
+  end
 end
 
-base10_to_base2(8)  # => 100
+def base10_to_base2(base_10_num)
+  power = highest_power_of_2(base_10_num)
+  bin_arr = Array.new(power+1)
+  bin_arr.fill(0)
+  bin_arr[power] = 1
+  left_over = base_10_num - 2**power
+
+  loop do
+    break if left_over == 0
+    power = highest_power_of_2(left_over)
+    bin_arr[power] = 1
+    left_over -= 2**power
+  end
+
+  bin_arr.reverse
+end
+
+puts consecutive_ones(base10_to_base2(5)).max
+puts consecutive_ones(base10_to_base2(13)).max
