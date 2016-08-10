a5efc3061ad6c0fd819b4a34cfea9ec3e937aa9a
jb2718-SE <jbattle2718@gmail.com>
2016-08-10 10:37:32 -0400

    added function to tally number of consecutive ones in a base-2 number

--- binary.rb
+++ binary.rb
@@ -1,5 +1,27 @@
+require 'pry'
 # Problem
 #-------------------
 # Given a base-10 integer, n, convert it to binary (base-2). 
 # Then find and print the base-10 integer denoting the 
 # maximum number of consecutive 1's in n's binary representation.
+
+def consecutive_ones(num)
+  tally = []
+  count = 0
+  digits = num.to_s.chars.map(&:to_i)
+  digits.each do |digit|
+    if digit == 0
+      tally << count
+      count = 0
+    else
+      count += 1
+    end
+  end
+  tally << count unless count == 0
+  tally
+end
+
+def base10_to_base2(base_10_num)
+end
+
+base10_to_base2(8)  # => 100
